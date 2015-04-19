e1ecbd762be1e17d6ed9e48113d4527b965c1de1
valeriy <valeriy.shkolniy@gmail.com>
2015-04-19 14:13:01 +0300

    fix favicon & add google-map & elements styling

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ process.env.PORT = 3000;
 //app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/webapp/resources/images/favicon.png'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- webapp/controllers/MainCtrl.js
+++ webapp/controllers/MainCtrl.js
@@ -4,4 +4,30 @@ angular.module('MainCtrl', [
 ]).controller('MainController', ['$scope', '$rootScope', function($scope, $rootScope) {
     $scope.prices = PRICES;
     console.log('$scope in MainController', $scope);
+
+    if(window.location.hash.indexOf('contacts') != -1){
+        function displayMap() {
+
+            var mapOptions = {
+
+                center: new google.maps.LatLng(49.7859605, 30.0956847),
+
+                zoom: 15,
+
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+
+            };
+
+            var mapObject = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+            var marker = new google.maps.Marker({
+                position: mapOptions.center,
+                title:"Дымоходы"
+            });
+
+            // To add the marker to the map, call setMap();
+            marker.setMap(mapObject);
+        }
+        displayMap();
+    }
 }]);
